149d7fe509b96c3d76d20ee358728ede344f0bdb
Samuel Omoding <samuel.omoding@student.moringaschool.com>
2023-12-13 12:55:00 +0300

    Completed assignment

---
+++ func.js
@@ -0,0 +1,32 @@
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+};
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2);
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function(integer) {
+    return function(fare) {
+        return (fare*integer);
+    };
+};
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function(drivers, driversSelector) {
+    return driversSelector(drivers);
+};
+
+
+    returnFirstTwoDrivers();
+    returnLastTwoDrivers();
+    selectingDrivers();
+    createFareMultiplier();
+    fareDoubler();
+    fareTripler();
+    selectDifferentDrivers();
